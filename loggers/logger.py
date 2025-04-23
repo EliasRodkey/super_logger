cdb17ca5273d49aafd25a5cfc962ebcdadf6896c
Elias Rodkey <elias.rodkey@gmail.com>
2025-04-22 17:27:18 -0700

    Adds dictionary like behavior to Logger

--- loggers/logger.py
+++ loggers/logger.py
@@ -434,6 +434,33 @@ class Logger:
         """
         self.logger.critical(message)
 
+    
+    @classmethod
+    def loggers(cls) -> list:
+        """
+        Returns the keys of the _instances dictionary
+
+        Returns:
+            list[str] logger names
+        """
+        return cls._instances.keys()
+    
+
+    @classmethod
+    def __getitem__(cls, name: str) -> 'Logger':
+        """
+        Allows for easy access to the logger instance using the class name as a dictionary
+
+        Args:
+            cls: refers to the class itself, always an argument in the __new__ method for classes
+                (like self for __init__ method)  
+            name (str): the name of the logger instance being created, will be added to _instances{}
+        
+        Returns:
+            Logger instance
+        """
+        return cls._instances[name]
+
 
 if __name__ == "__main__":
 
